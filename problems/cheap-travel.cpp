27c96e5004faa006d29efe0c3952aa7956206568
Obaida <91075544+Abdullah-Wael-Ahmed@users.noreply.github.com>
2025-11-30 23:30:28 +0200

    Create cheap-travel.cpp

---
+++ problems/cheap-travel.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int trips, tripsCoveredByTicket;
+    float singleTripPrice, ticketPrice;
+
+    cin >> trips >> tripsCoveredByTicket >> singleTripPrice >> ticketPrice;
+
+    if ((ticketPrice / tripsCoveredByTicket) < singleTripPrice) {
+
+        int remainingTrips = trips % tripsCoveredByTicket;
+        int noTickets = (trips - remainingTrips) / tripsCoveredByTicket;
+        long sum = noTickets * ticketPrice;
+
+        if (remainingTrips > 0 ){
+            if (remainingTrips * singleTripPrice > ticketPrice) {
+                sum += ticketPrice;
+            } else {
+                sum += remainingTrips * singleTripPrice;
+            }
+        }
+
+        cout << sum;
+
+    } else {
+        long sum = trips * singleTripPrice;
+        cout << sum;
+    }
+
+    return 0;
+}
